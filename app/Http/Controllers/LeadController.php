81e22855b9271efc81829b51405a0c4400d3c132
waras216 <israel.2014flores@gmail.com>
2026-06-29 00:12:41 -0500

    migracion pc

--- app/Http/Controllers/LeadController.php
+++ app/Http/Controllers/LeadController.php
@@ -13,11 +13,21 @@ class LeadController extends Controller
      */
     public function index(Request $request)
     {
-        return response()->json(
-            Lead::where('id_tenant',$request->user()->id_tenant)
-            ->with(['cliente', 'usuario'])
-            ->get()
-        );
+        $lead = Lead::where('id_tenant', $request->user()->id_tenant)
+            ->with(['cliente', 'usuario']);
+
+
+        $lead->when($request->filled('buscar'), function ($q) use($request){
+            $q->where('titulo', 'like', '%' . $request->search . '%');
+        });
+
+        $lead->when($request->filled('filterEstatus') && $request->filterEstatus !== 'todos', function ($q) use ($request){
+            $q->where('estado', $request->filterEstatus);
+        });
+        
+        $lead->latest('id_lead');
+
+        return response()->json($lead->paginate(15));
     }
 
     /**
@@ -47,7 +57,7 @@ class LeadController extends Controller
     {
         return response()->json(
             Lead::where('id_lead', $id)
-            ->where('id_tenant', $request->user()->id_lealtad)
+            ->where('id_tenant', $request->user()->id_tenant)
             ->with(['cliente', 'usuario', 'actividades'])
             ->firstOrFail()
         );        
